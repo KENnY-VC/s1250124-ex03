f46fea332fbfa102301bd79da6797fca080ead83
s1250124 <s1250124@std3dc6>
2018-07-02 13:51:29 +0900

    Won/lose message added

--- DiceGame.c
+++ DiceGame.c
@@ -4,8 +4,14 @@
 
 int main(){
   int d1, d2;
+  char name[32];
 
   srand(time(NULL));
+
+  printf("What your name?\n");
+  printf("> ");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
 
   printf("Rolling the dice...\n");
 
@@ -15,6 +21,12 @@ int main(){
   printf("Die 1: %d\n",d1);
   printf("Die 2: %d\n",d2);
   printf("Total value: %d\n",d1+d2);
+
+  if(d1+d2>7)
+    printf("%s won!\n",name);
+
+  else
+    printf("%s lose...\n",name);
 
   return 0;
 }
